7c4a57fa163ba03443c0d565e2f22c4ac7edeb14
Sachin Lokesh <sachinlokesh@Sachins-MacBook-Air.local>
2024-07-30 02:42:17 +0530

    Initial commit: Basic project structure for BIBot.brd.v1

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>BIBot</h1>
+      <p>Welcome to the BIBot chat application!</p>
+    </div>
+  );
+}
+
+export default App;
